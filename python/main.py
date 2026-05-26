a9f732aaa9c112392e020d88d0cd2f50eab93467
Raul Azzi Corsi <raul.acorsi@gmail.com>
2026-05-26 19:36:31 -0300

    Add: Try and input sanitation for each case

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 #Imports:
 from pyfiglet import figlet_format
+from risk_model import troop_risk, air_raid_risk, missile_strike, total_risk
 
 
 
@@ -11,14 +12,19 @@ def separador():
     print("═" * 53 + "\n")
 
 def pause():
-    input("\nPressione Enter para voltar ao menu...")
+    input("Press enter to return...\n")
+
+
+
 
 #Main func:
 def main():
-        separador()
-        print(figlet_format("OrbitGuard"))
+     usr_lat = None
+     usr_lon = None
 
-        while True:
+     separador()
+     print(figlet_format("OrbitGuard"))
+     while True:
           separador()
           print("""
           [1] About this system
@@ -32,15 +38,71 @@ def main():
           usr_choice = input("Insert choice: ")
           match usr_choice:
                case "1":
-                    println("case 1")
+                    separador()
+                    print("""   ABOUT ORBITGUARD
+                          
+   OrbitGuard is a cli application that aims to 
+   centralize all OSINT available data inside the 
+   territory of Ukraine in order to help civilians 
+   to find resources, safe zones and to alert of 
+   any danger in the horizon.
+                          """)
+                    separador()
+                    pause()
+
+
                case "2":
-                    println("case 2")
+                    try:
+                         if usr_lat is None or usr_lon is None: 
+                              usr_lat = float(input("Insert your latitue: E.g. -23.5505 | "))
+                              usr_lon = float(input("Insert your longitude: E.g. 74.0060 | "))
+                         else:
+                              print("case 2")
+                              break
+                    except ValueError:
+                         println("Invalid input - please enter a valid coordinate. E.g. -23.5505")
+                         pause()
+
+
                case "3":
-                    println("case 3")
+                    try:
+                         if usr_lat is None or usr_lon is None: 
+                              usr_lat = float(input("Insert your latitue: E.g. -23.5505 | "))
+                              usr_lon = float(input("Insert your longitude: E.g. 74.0060 | "))
+                         else:
+                              print("case 3")
+                              break
+                    except ValueError:
+                         println("Invalid input - please enter a valid coordinate. E.g. -23.5505")
+                         pause()
+
+
                case "4":
-                    println("case 4")
+                    try:
+                         if usr_lat is None or usr_lon is None: 
+                              usr_lat = float(input("Insert your latitue: E.g. -23.5505 | "))
+                              usr_lon = float(input("Insert your longitude: E.g. 74.0060 | "))
+                         else:
+                              print("case 4")
+                              break
+                    except ValueError:
+                         println("Invalid input - please enter a valid coordinate. E.g. -23.5505")
+                         pause()
+
+
                case "5":
-                    println("case 5")
+                    try:
+                         if usr_lat is None or usr_lon is None: 
+                              usr_lat = float(input("Insert your latitue: E.g. -23.5505 | "))
+                              usr_lon = float(input("Insert your longitude: E.g. 74.0060 | "))
+                         else:
+                              print("case 5")
+                              break
+                    except ValueError:
+                         println("Invalid input - please enter a valid coordinate. E.g. -23.5505")
+                         pause()
+
+          
                case "0":
                     break
                case _:
